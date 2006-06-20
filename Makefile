566d88ba33d8666ddf3f9c3970e4d6a37a530d4a
csl <csl@a082123e-ac16-0410-b537-addfd1976b9b>
2006-06-20 18:46:47 +0000

    Added files.
    git-svn-id: svn://svn.code.sf.net/p/jp2a/code@1 a082123e-ac16-0410-b537-addfd1976b9b

---
+++ Makefile
@@ -0,0 +1,15 @@
+# These directories must include jpeglib.h
+# http://www.ijg.org
+
+INCLUDE = /sw/include
+LIBDIR = /sw/lib
+
+CXXFLAGS = -g -Os -W -Wall -I$(INCLUDE)
+LDFLAGS = -L$(LIBDIR) -ljpeg
+
+TARGETS = jp2a
+
+all: $(TARGETS)
+
+clean:
+	rm -f $(TARGETS)
---
+++ jp2a.cpp
@@ -0,0 +1,215 @@
+/*
+ * Copyright (C) 2006 Christian Stigen Larsen <http://csl.sublevel3.org>
+ * Distributed under the BSD License
+ *
+ * Compilation hint:
+ * g++ test.cpp -I/sw/include -L/sw/lib -ljpeg
+ *
+ * $Id$
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <memory.h>
+#include <jpeglib.h>
+
+bool verbose = false;
+int width = 80;
+int height = 25;
+
+const char* license = "Copyright (C) 2006 Christian Stigen Larsen.\nDistributed under the BSD license";
+
+// Printable ASCII characters, sorted least intensive to most intensive
+const char* origascii = "   ........oooOOOOOOO";
+char ascii[1024];
+
+int test_decompress(const char* file);
+
+void help() {
+	fprintf(stderr, "Usage: jp2afilename[s].jpg\n\n");
+	fprintf(stderr, "Simple JPEG to ASCII converter\n\n");
+	fprintf(stderr, "OPTIONS\n");
+	fprintf(stderr, "    -h, --help       Print program help\n");
+	fprintf(stderr, "    --size=WxH       Set output ASCII width and height, default is %dx%d\n", width, height);
+	fprintf(stderr, "    --chars=...      Select character palette used to paint the image.  Leftmost character\n");
+	fprintf(stderr, "                     corresponds to black pixel, rightmost to white pixel.\n");
+	fprintf(stderr, "    -v, --verbose    Verbose output\n");
+	fprintf(stderr, "\n");
+	fprintf(stderr, "EXAMPLE\n");
+	fprintf(stderr, "   jpg2ascii --size=80x25 --chars='...oooxx@@' somefile.jpg\n\n");
+	fprintf(stderr, "%s\n", license);
+	exit(1);
+}
+
+void parse_options(int argc, char** argv) {
+	strcpy(ascii, origascii);
+
+	for ( int n=1; n<argc; ++n ) {
+		if ( argv[n][0] != '-')
+			continue;
+
+		int hits = 0;
+
+		if ( !strcmp(argv[n], "-h") || !strcmp(argv[n], "--help") ) help();
+		if ( !strcmp(argv[n], "-v") || !strcmp(argv[n], "--verbose") ) {
+			++hits;
+			verbose = true;
+		}
+
+		hits += sscanf(argv[n], "--size=%dx%d", &width, &height);
+		hits += sscanf(argv[n], "--chars=%1000s", ascii);
+
+		if ( hits == 0 ) {
+			fprintf(stderr, "Unknown option %s\n\n", argv[n]);
+			help();
+		}
+	}
+
+	// check options
+	if ( strlen(ascii) == 0 )
+		strcpy(ascii, origascii);
+
+	if ( width <= 1 ) width = 1;
+	if ( height <= 1 ) height = 1;
+}
+
+int main(int argc, char** argv) {
+	if ( argc<2 )
+		help();
+
+	parse_options(argc, argv);
+
+	for ( int n=1; n<argc; ++n ) {
+		if ( argv[n][0] != '-')
+			test_decompress(argv[n]);
+	}
+
+	return 0;
+}
+
+int test_decompress(const char* file) {
+	FILE * infile;
+
+        if ( (infile = fopen(file, "rb")) == NULL ) {
+            fprintf(stderr, "can't open %s\n", file);
+            return 1;
+        }
+
+        struct jpeg_error_mgr jerr;
+	struct jpeg_decompress_struct cinfo;
+
+        cinfo.err = jpeg_std_error(&jerr);
+
+        jpeg_create_decompress(&cinfo);
+        jpeg_stdio_src(&cinfo, infile);
+	jpeg_read_header(&cinfo, TRUE);
+
+	jpeg_start_decompress(&cinfo);
+
+	int row_stride = cinfo.output_width * cinfo.output_components;
+	JSAMPARRAY buffer = (*cinfo.mem->alloc_sarray) ((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
+
+	double accum[width];
+
+	int sizeAscii = strlen(ascii) - 1;
+	int pixelsPerChar = (row_stride/3) / width;
+	if ( pixelsPerChar <= 0 ) pixelsPerChar = 1;
+
+	int linesToAdd = cinfo.output_height / height;
+	if ( linesToAdd <= 0 ) linesToAdd = 1;
+
+	if ( verbose ) {
+		printf("Filename: %s\n", file);
+		printf("\n");
+		printf("Output width : %d\n", width);
+		printf("Output height: %d\n", height);
+		printf("\n");
+		printf("Source width : %d\n", cinfo.output_width);
+		printf("Source height: %d\n", cinfo.output_height);
+		printf("\n");
+		printf("ASCII characters used for printing: %d\n", 1 + sizeAscii);
+		printf("Pixels per character: %d\n", pixelsPerChar);
+		printf("Lines per character : %d\n", linesToAdd);
+		printf("\n");
+	}
+
+	int linesAdded = 0;
+
+	while ( cinfo.output_scanline < cinfo.output_height ) {
+		jpeg_read_scanlines(&cinfo, buffer, 1);
+
+		int currChar = 0;
+		int pixelsAdded = 0;
+		for ( int pixel=0; pixel < row_stride; pixel += 3) {
+			int r = buffer[0][pixel];
+			int g = buffer[0][pixel + 1];
+			int b = buffer[0][pixel + 2];
+
+			accum[currChar] += static_cast<double>( (r + g + b) / (3.0f * 255.0f) );
+			++pixelsAdded;
+
+			if ( pixelsAdded >= pixelsPerChar ) {
+				if ( currChar>=width ) break;
+				// normalize
+				accum[currChar] /= static_cast<double>(pixelsAdded);
+				pixelsAdded = 0;
+				++currChar;
+			}
+		}
+
+		++linesAdded;
+
+		if ( linesAdded > linesToAdd ) {
+			printf("|");
+
+			for ( int n=0; n < width; ++n ) {
+//				accum[n] /= linesAdded;
+
+				int pos = static_cast<int>( (double)( (1.0f - accum[n]) * sizeAscii) );
+
+				if ( pos < 0 ) pos = 0;
+				if ( pos > sizeAscii ) pos = sizeAscii;
+
+				printf("%c", ascii[pos]);
+
+				// clear at the same time
+				accum[n] = 0.0f;
+			}
+
+			printf("|\n");
+			fflush(stdout);
+
+			linesAdded = 0;
+		} else {
+			if ( (1 + cinfo.output_scanline) == cinfo.output_height ) {
+				// print what we have so far
+				printf("|");
+
+				for ( int n=0; n < width; ++n ) {
+	//				accum[n] /= linesAdded;
+
+					int pos = static_cast<int>( (double)( (1.0f - accum[n]) * sizeAscii) );
+
+					if ( pos < 0 ) pos = 0;
+					if ( pos > sizeAscii ) pos = sizeAscii;
+
+					printf("%c", ascii[pos]);
+
+					// clear at the same time
+					accum[n] = 0.0f;
+				}
+
+				printf("|\n");
+				fflush(stdout);
+			}
+		}
+
+	}
+
+	jpeg_finish_decompress(&cinfo);
+	jpeg_destroy_decompress(&cinfo);
+	fclose(infile);
+
+	return 0;
+}
