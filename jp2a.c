de5c9f521d9e7d266f1435ed0380ccd26c8d95a8
csl <csl@a082123e-ac16-0410-b537-addfd1976b9b>
2006-06-22 12:41:29 +0000

    Code cleanup in jp2a.c, can also read from stdin
    git-svn-id: svn://svn.code.sf.net/p/jp2a/code@14 a082123e-ac16-0410-b537-addfd1976b9b

--- jp2a.c
+++ jp2a.c
@@ -1,130 +1,149 @@
 /*
  * Copyright (C) 2006 Christian Stigen Larsen <http://csl.sublevel3.org>
- * Distributed under the BSD License
+ * Distributed under the BSD license.
  *
  * Compilation hint:
- * g++ test.cpp -I/sw/include -L/sw/lib -ljpeg
+ * cc test.cpp -I/sw/include -L/sw/lib -ljpeg
  *
  * $Id$
+ *
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 #include <jpeglib.h>
 
+const char* version   = "jp2a 1.0.0";
+const char* copyright = "Copyright (C) 2006 Christian Stigen Larsen";
+const char* license   = "Distributed under the BSD license";
+
+const char* default_palette = "   ...',;:clodxkO0KXNWM";
+
+typedef struct Image_ {
+	int width;
+	int height;
+	double *p;
+} Image;
+
+// Options
 int verbose = 0;
 int color = 0;
 int width = 80;
 int height = 25;
-char ascii[257];
 
-const char* license = "Copyright (C) 2006 Christian Stigen Larsen.\nDistributed under the BSD license";
-
-// Printable ASCII characters, sorted least intensive to most intensive
-const char* origascii = "   ...',;:clodxkO0KXNWM";
+char ascii_palette[257] = "";
 
 void help() {
-	fprintf(stderr, "Usage: jp2afilename[s].jpg\n\n");
+	fprintf(stderr, "%s\n", version);
+	fprintf(stderr, "Usage: jp2a file.jpg [file2.jpg [...]]\n\n");
 	fprintf(stderr, "Simple JPEG to ASCII converter\n\n");
+
 	fprintf(stderr, "OPTIONS\n");
+	fprintf(stderr, "    -                Decompress JPG from stdin\n");
 	fprintf(stderr, "    -h, --help       Print program help\n");
 	fprintf(stderr, "    --size=WxH       Set output ASCII width and height, default is %dx%d\n", width, height);
 	fprintf(stderr, "    --chars=...      Select character palette used to paint the image.  Leftmost character\n");
 	fprintf(stderr, "                     corresponds to black pixel, rightmost to white pixel.\n");
 	fprintf(stderr, "    -v, --verbose    Verbose output\n");
+	fprintf(stderr, "    -V, --version    Show program version\n");
 	fprintf(stderr, "\n");
-	fprintf(stderr, "EXAMPLE\n");
-	fprintf(stderr, "   jpg2ascii --size=80x25 --chars='...oooxx@@' somefile.jpg\n\n");
+
+	fprintf(stderr, "EXAMPLES\n");
+	fprintf(stderr, "   jp2a --size=80x25 --chars='...oooxx@@' somefile.jpg\n");
+	fprintf(stderr, "   cat picture.jpg | jp2a - --size=100x100\n\n");
+
+	fprintf(stderr, "%s\n", copyright);
 	fprintf(stderr, "%s\n", license);
 	exit(1);
 }
 
 void parse_options(int argc, char** argv) {
-	strcpy(ascii, origascii);
-
+	if ( argc<2 ) help();
 	int n;
+
 	for ( n=1; n<argc; ++n ) {
-		if ( argv[n][0] != '-')
-			continue;
+		char *s = argv[n];
+
+		if ( *s != '-' ) continue;
 
 		int hits = 0;
 
-		if ( !strcmp(argv[n], "-h") || !strcmp(argv[n], "--help") ) help();
-		if ( !strcmp(argv[n], "-v") || !strcmp(argv[n], "--verbose") ) {
-			++hits;
+		if ( !strcmp(s, "-h") || !strcmp(s, "--help") )
+			help();
+
+		if ( !strcmp(s, "-v") || !strcmp(s, "--verbose") ) {
 			verbose = 1;
+			++hits;
 		}
 
-		hits += sscanf(argv[n], "--size=%dx%d", &width, &height);
-		hits += sscanf(argv[n], "--chars=%256s", ascii);
+		if ( !strcmp(s, "-V") || !strcmp(s, "--version") ) {
+			fprintf(stderr, "%s\n\n", version);
+			fprintf(stderr, "%s\n", copyright);
+			fprintf(stderr, "%s\n", license);
+			exit(0);
+		}
 
-		if ( hits == 0 ) {
-			fprintf(stderr, "Unknown option %s\n\n", argv[n]);
+		hits += sscanf(s, "--size=%dx%d", &width, &height);
+		hits += sscanf(s, "--chars=%256s", ascii_palette);
+
+		if ( !hits ) {
+			fprintf(stderr, "Unknown option %s\n\n", s);
 			help();
 		}
 	}
 
-	// check options
-	if ( strlen(ascii) == 0 )
-		strcpy(ascii, origascii);
+	// Palette must be at least two characters
+	if ( ascii_palette[0] == 0 ) strcpy(ascii_palette, default_palette);
+	if ( ascii_palette[1] == 0 ) strcpy(ascii_palette, default_palette);
 
 	if ( width <= 1 ) width = 1;
 	if ( height <= 1 ) height = 1;
 }
 
-void print(double* accum, int width, int sizeAscii) {
-	int n;
+void print(Image* i, int sizeAscii) {
+	#define ROUND(x) ( 0.5 + x )
 	char buf[width+1];
+	int n;
 
-	for ( n=0; n < width; ++n ) {
-		int pos = (int) round(sizeAscii * accum[n]);
+	for ( n=0; n < i->width; ++n ) {
+		int pos = (int) ROUND( sizeAscii * i->p[n] );
 
-		// the following should never happen
+		// The following should never happen
 		if ( pos < 0 ) pos = 0;
 		if ( pos > sizeAscii ) pos = sizeAscii;
 
-		buf[n] = ascii[pos];
+		buf[n] = ascii_palette[pos];
 	}
 
 	buf[width] = 0;
 	puts(buf);
 }
 
-void invert(double* accum, int width) {
+void invert(Image* i) {
 	int n;
-	for ( n=0; n<width; ++n )
-		accum[n] = 1.0 - accum[n];
+	for ( n=0; n < i->width; ++n )
+		i->p[n] = 1.0 - i->p[n];
 }
 
-void clear(double* accum, int width) {
-	int n;
-	for ( n=0; n<width; ++n )
-		accum[n] = 0.0;
+void clear(Image* i) {
+	memset(i->p, 0, i->width * sizeof(double) );
 }
 
-void normalize(double* accum, int width, double factor) {
+void normalize(Image* i, double factor) {
 	int n;
-	for ( n=0; n<width; ++n )
-		accum[n] /= factor;
+	for ( n=0; n < i->width; ++n )
+		i->p[n] /= factor;
 }
 
-int test_decompress(const char* file) {
-	FILE * infile;
-
-        if ( (infile = fopen(file, "rb")) == NULL ) {
-            fprintf(stderr, "can't open %s\n", file);
-            return 1;
-        }
-
+int decompress(FILE *fp) {
         struct jpeg_error_mgr jerr;
 	struct jpeg_decompress_struct cinfo;
 
         cinfo.err = jpeg_std_error(&jerr);
 
         jpeg_create_decompress(&cinfo);
-        jpeg_stdio_src(&cinfo, infile);
+        jpeg_stdio_src(&cinfo, fp);
 	jpeg_read_header(&cinfo, TRUE);
 
 	jpeg_start_decompress(&cinfo);
@@ -132,9 +151,17 @@ int test_decompress(const char* file) {
 	int row_stride = cinfo.output_width * cinfo.output_components;
 	JSAMPARRAY buffer = (*cinfo.mem->alloc_sarray) ((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
 
-	double accum[width];
+	Image image;
+	image.width = width;
+	image.height = height;
+	image.p = malloc( width * sizeof(double) );
 
-	int sizeAscii = strlen(ascii) - 1;
+	if ( image.p == NULL ) {
+		fprintf(stderr, "Could not allocate %ld bytes for output image", width * sizeof(double) );
+		exit(1);
+	}
+
+	int sizeAscii = strlen(ascii_palette) - 1;
 	int comps = cinfo.out_color_components;
 
 	int pixelsPerChar = row_stride / (comps * width);
@@ -144,23 +171,19 @@ int test_decompress(const char* file) {
 	if ( linesToAdd <= 0 ) linesToAdd = 1;
 
 	if ( verbose ) {
-		printf("Filename: %s\n", file);
-		printf("\n");
-		printf("Output width : %d\n", width);
-		printf("Output height: %d\n", height);
-		printf("\n");
-		printf("Source width : %d\n", cinfo.output_width);
-		printf("Source height: %d\n", cinfo.output_height);
-		printf("\n");
-		printf("ASCII characters used for printing: %d\n", 1 + sizeAscii);
-		printf("Pixels per character: %d\n", pixelsPerChar);
-		printf("Lines per character : %d\n", linesToAdd);
-		printf("Color components    : %d\n", comps);
-		printf("\n");
+		fprintf(stderr, "Output width : %d\n", width);
+		fprintf(stderr, "Output height: %d\n", height);
+		fprintf(stderr, "Source width : %d\n", cinfo.output_width);
+		fprintf(stderr, "Source height: %d\n", cinfo.output_height);
+		fprintf(stderr, "ASCII characters used for printing: %d\n", 1 + sizeAscii);
+		fprintf(stderr, "Pixels per character: %d\n", pixelsPerChar);
+		fprintf(stderr, "Lines per character : %d\n", linesToAdd);
+		fprintf(stderr, "Color components    : %d\n", comps);
+		fprintf(stderr, "\n");
 	}
 
 	int linesAdded = 0;
-	clear(accum, width);
+	clear(&image);
 
 	while ( cinfo.output_scanline < cinfo.output_height ) {
 		jpeg_read_scanlines(&cinfo, buffer, 1);
@@ -173,7 +196,8 @@ int test_decompress(const char* file) {
 
 			int addit;
 			for ( addit=0; addit<comps; ++addit )
-				accum[currChar] += (double) buffer[0][pixel + addit] / (comps * 255.0);
+				image.p[currChar] += (double) buffer[0][pixel + addit] / (comps * 255.0);
+				//accum[currChar] += (double) buffer[0][pixel + addit] / (comps * 255.0);
 
 			++pixelsAdded;
 
@@ -189,33 +213,49 @@ int test_decompress(const char* file) {
 		++linesAdded;
 
 		if ( linesAdded > linesToAdd ) {
-			normalize(accum, width, pixelsAdded * linesAdded);
-			invert(accum, width);
-			print(accum, width, sizeAscii);
-			clear(accum, width);
+			///normalize(accum, width, pixelsAdded * linesAdded);
+			normalize(&image, pixelsAdded * linesAdded);
+			//invert(accum, width);
+			invert(&image);
+			//print(accum, width, sizeAscii);
+			print(&image, sizeAscii);
+			//clear(accum, width);
+			clear(&image);
 			linesAdded = 0;
 		}
 	}
 
 	jpeg_finish_decompress(&cinfo);
 	jpeg_destroy_decompress(&cinfo);
-	fclose(infile);
+	free(image.p);
 	return 0;
 }
 
 int main(int argc, char** argv) {
-	if ( argc<2 )
-		help();
-
 	parse_options(argc, argv);
 
 	int n;
 	for ( n=1; n<argc; ++n ) {
-		if ( argv[n][0] != '-')
-			test_decompress(argv[n]);
+
+		// Skip options
+		if ( argv[n][0]=='-' && argv[n][1]!=0 )
+			continue;
+
+		if ( argv[n][0]=='-' && argv[n][1]==0 ) {
+			decompress(stdin);
+			continue;
+		}
+
+		FILE *fp;
+		if ( (fp = fopen(argv[n], "rb")) != NULL ) {
+			if ( verbose ) fprintf(stderr, "File: %s\n", argv[n]);
+			decompress(fp);
+			fclose(fp);
+		} else {
+			fprintf(stderr, "Can't open %s\n", argv[n]);
+			return 1;
+		}
 	}
 
 	return 0;
 }
-
-
