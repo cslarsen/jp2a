a861c69a9dc5d0b6befc93ee3b22ae724cc69d3b
csl <csl@a082123e-ac16-0410-b537-addfd1976b9b>
2006-07-13 17:32:21 +0000

    Minor cleanup in html.c
    git-svn-id: svn://svn.code.sf.net/p/jp2a/code@208 a082123e-ac16-0410-b537-addfd1976b9b

--- src/html.c
+++ src/html.c
@@ -10,26 +10,24 @@
 #include <stdio.h>
 
 void print_html_start(const int fontsize) {
-	printf("<?xml version='1.0' encoding='ISO-8859-1'?>\n"
-	"<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN'"
-	"  'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>\n"
-	"<html xmlns='http://www.w3.org/1999/xhtml' lang='en' xml:lang='en'>\n"
-	"<head>\n"
-	"<title>jp2a converted image</title>\n"
-	"<style type='text/css'>\n"
-	".ascii {\n"
-	"   font-size:%dpt;\n"
-	"}\n"
-	"</style>\n"
-	"</head>\n"
-	"<body>\n"
-	"<div class='ascii'>\n"
-	"<pre>\n", fontsize);
+	
+	puts(   "<?xml version='1.0' encoding='ISO-8859-1'?>\n"
+		"<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN'"
+		"  'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>\n"
+		"<html xmlns='http://www.w3.org/1999/xhtml' lang='en' xml:lang='en'>\n"
+		"<head>\n"
+		"<title>jp2a converted image</title>\n"
+		"<style type='text/css'>\n"
+		".ascii {");
+	printf( "   font-size:%dpt;\n", fontsize);
+	puts(   "}\n"
+		"</style>\n"
+		"</head>\n"
+		"<body>\n"
+		"<div class='ascii'>\n"
+		"<pre>");
 }
 
 void print_html_end() {
-	printf("</pre>\n");
-	printf("</div>\n");
-	printf("</body>\n");
-	printf("</html>\n");
+	puts("</pre>\n</div>\n</body>\n</html>");
 }
