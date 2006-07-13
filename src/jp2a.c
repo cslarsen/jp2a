2d256e54777534ff49df89ecefbea3a785125368
csl <csl@a082123e-ac16-0410-b537-addfd1976b9b>
2006-07-13 09:37:38 +0000

    Mark this release, this version has a huge difference from the last checking (int output quality), but images actually look a bit better (i _think_)
    git-svn-id: svn://svn.code.sf.net/p/jp2a/code@155 a082123e-ac16-0410-b537-addfd1976b9b

--- src/jp2a.c
+++ src/jp2a.c
@@ -432,13 +432,11 @@ int curl_download(const char* url, const int debug) {
 #endif
 
 void process_scanline(const struct jpeg_decompress_struct *jpg, const JSAMPLE* scanline, Image* image) {
-	static int lasty = -1;
+	static int lasty = 0;
 	const int y = ROUND(image->to_dst_y * (float) (jpg->output_scanline-1));
 
 	// include all scanlines since last y-stepping
-	while ( lasty != y ) {
-		++lasty;
-
+	while ( lasty <= y ) {
 		const int y_w = lasty * image->width;
 		int x;
 
@@ -449,6 +447,7 @@ void process_scanline(const struct jpeg_decompress_struct *jpg, const JSAMPLE* s
 		}
 
 		++image->yadds[lasty];
+		++lasty;
 	}
 
 	lasty = y;
