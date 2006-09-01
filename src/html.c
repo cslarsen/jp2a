ac4a3b1e80a7067fec80889f63b5c95fa91a0f8d
csl <csl@a082123e-ac16-0410-b537-addfd1976b9b>
2006-09-01 10:52:47 +0000

    Removed 'GPL v2 or later' to 'GPL v2'
    git-svn-id: svn://svn.code.sf.net/p/jp2a/code@439 a082123e-ac16-0410-b537-addfd1976b9b

--- src/html.c
+++ src/html.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2006 Christian Stigen Larsen, http://csl.sublevel3.org
- * Distributed under the GNU General Public License (GPL) v2 or later.
+ * Distributed under the GNU General Public License (GPL) v2.
  *
  * Project homepage on http://jp2a.sf.net
  *
--- src/options.c
+++ src/options.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2006 Christian Stigen Larsen, http://csl.sublevel3.org
- * Distributed under the GNU General Public License (GPL) v2 or later.
+ * Distributed under the GNU General Public License (GPL) v2.
  *
  * Project homepage on http://jp2a.sf.net
  *
@@ -84,7 +84,7 @@ const char *fileout = "-"; // stdout
 
 const char* version   = PACKAGE_STRING;
 const char* copyright = "Copyright (C) 2006 Christian Stigen Larsen";
-const char* license   = "Distributed under the GNU General Public License (GPL) v2 or later.";
+const char* license   = "Distributed under the GNU General Public License (GPL) v2.";
 const char* url       = "http://jp2a.sf.net";
 
 void print_version() {
